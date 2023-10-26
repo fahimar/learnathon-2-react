b11afe8184d345bd720f48142ca623fb48dceb3a
fahimar <fahimar14040@gmail.com>
2023-10-27 01:24:20 +0600

    Basic Component and function in React.

---
+++ class-2/my-app/src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import { BasicClassComponent } from "./components/BasicClassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to React second class - Learnathon 2.0</p>
+        <BasicClassComponent></BasicClassComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ class-2/my-app/src/components/BasicClassComponent.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+// React component names should be in PascalCase
+export class BasicClassComponent extends Component {
+  render() {
+    return <h1>This is a simple header from Class Component</h1>;
+  }
+}
+
+// export default basicClasscomponent;
