c67c6fcc2b830db0301b36580a6a9d812401728f
fahimar <fahimar14040@gmail.com>
2023-10-29 13:36:50 +0600

    console.error     Warning: validateDOMNesting(...): <body> cannot appear as a child of <div>.         at body         at div         at App
          3 |
          4 | test("renders learn react link", () => {
        > 5 |   render(<App />);
            |         ^
          6 |   const linkElement = screen.getByText(/learn react/i);
          7 |   expect(linkElement).toBeInTheDocument();
          8 | }); facing some problem.

--- class-2/my-app/src/App.js
+++ class-2/my-app/src/App.js
@@ -1,14 +1,24 @@
 // import logo from "./logo.svg";
 import "./App.css";
+//import { BasicClassComponent } from "./components/BasicClassComponent";
+
 import { BasicClassComponent } from "./components/BasicClassComponent";
 
+//E:\VivaSoft Frontend Developer\Learnathon 2.0\Learnathon 2.0 REACT\learnathon-2-react\class-2\my-app\src\components\BasicClassComponent.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to React second class - Learnathon 2.0</p>
-        <BasicClassComponent></BasicClassComponent>
-      </header>
+    <div>
+      <body>
+        <div>
+          <div className="App">
+            <header className="App-header">
+              <p>Welcome to React second class - Learnathon 2.0</p>
+              <h1>This is a simple header from Class Component</h1>
+              <BasicClassComponent></BasicClassComponent>
+            </header>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
--- class-2/my-app/src/components/BasicClassComponent.js
+++ class-2/my-app/src/components/BasicClassComponent.js
@@ -1,9 +1,11 @@
+// import React from "react";
 import { Component } from "react";
 // React component names should be in PascalCase
+
 export class BasicClassComponent extends Component {
   render() {
     return <h1>This is a simple header from Class Component</h1>;
   }
 }
 
-// export default basicClasscomponent;
+//export default BasicClassComponent;
