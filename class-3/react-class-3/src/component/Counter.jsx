70c282784e105f40ee37cfc158e98d5b44a34d6f
fahimar <fahimar14040@gmail.com>
2023-11-06 23:42:15 +0600

    CSS-in-JS(Styled-component)

--- class-3/react-class-3/src/component/Counter.jsx
+++ class-3/react-class-3/src/component/Counter.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import styles from "./counter.module.css";
 import "./counter.scss";
-
+import { styled } from "styled-components";
+// import styled from "styled-components";
 const Counter = () => {
+  const Button = styled.button`
+    padding: 10px;
+    margin: 4px;
+    width: 100px;
+    border-radius: 5px;
+    color: blue;
+    background-color: white;
+  `;
+  // const Button = styled.button`
+  //   padding: 10px;
+  //   margin: 4px;
+  //   width: 100px;
+  //   border-radius: 5px;
+  //   color: blue;
+  //   background-color: red;
+  // `;
+
   return (
     <div className={styles.counter}>
       Counter
       <p>This is Counter</p>
-      <button className="button-primary item-1">Button 1</button>
+      {/* <button className="button-primary item-1">Button 1</button>*/}
+      <Button>Button 1</Button>
       <button className="button-secondary item-2">Button 2</button>
     </div>
   );
