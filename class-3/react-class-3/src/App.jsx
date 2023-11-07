dc546fe596b5c053c07f5595de969d6d9b080896
fahimar <fahimar14040@gmail.com>
2023-11-08 01:07:25 +0600

    Router commited work.

--- class-3/react-class-3/src/App.jsx
+++ class-3/react-class-3/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./App.css";
 import Counter from "./component/Counter";
+import Homepage from "./pages/Homepage";
+
+const routes = createBrowserRouter([
+  { path: "/", element: <Homepage /> },
+  { path: "/counter", element: <Counter /> },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <Counter />
-    </>
-  );
+  {
+    // return ();
+    /*<Counter />*/
+  }
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
---
+++ class-3/react-class-3/src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div> THis is a Homepage</div>;
+};
+
+export default Homepage;
