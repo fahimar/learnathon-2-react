937f7d438ca073487b1bf0a6a8d959b96b7fb229
fahimar <fahimar14040@gmail.com>
2023-11-08 21:23:01 +0600

    Dynamic Routing class-3

--- class-3/react-class-3/src/App.jsx
+++ class-3/react-class-3/src/App.jsx
@@ -4,10 +4,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Counter from "./component/Counter";
 import Homepage from "./pages/Homepage";
+import User from "./pages/User";
+import Users from "./pages/Users";
 
 const routes = createBrowserRouter([
   { path: "/", element: <Homepage /> },
   { path: "/counter", element: <Counter /> },
+  { path: "users", element: <Users /> },
+  {
+    path: "user/:id", // dynamic the path
+    element: <User />,
+  },
 ]);
 
 function App() {
