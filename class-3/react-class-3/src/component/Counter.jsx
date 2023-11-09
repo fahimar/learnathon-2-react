a6a8296ba1919b469086b9b70d91fb350178aa7a
fahimar <fahimar14040@gmail.com>
2023-11-09 21:31:06 +0600

    Adding dispatch in button.

--- class-3/react-class-3/src/component/Counter.jsx
+++ class-3/react-class-3/src/component/Counter.jsx
@@ -22,9 +22,15 @@ const Counter = () => {
   //   color: blue;
   //   background-color: red;
   // `;
+  const reducer = (state, action) => {
+    // update states
+    if (action === "increment") return state + 1;
+    else if (action === "decrement") return state - 1;
+    return state;
+  };
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState(null);
-  // const [value, dispatch] = useReducer(reducer, 0);
+  // const [value, setValue] = useState(null);
+  const [value, dispatch] = useReducer(reducer, 0);
   const handleCount = () => {
     // setCount(count + 1); // schedule
     // setCount(count + 1);
@@ -36,10 +42,10 @@ const Counter = () => {
   };
   useEffect(() => {
     console.log("I am in useEffect");
-    const intervalId = setInterval(() => console.log("I am effect"), 100);
-    return () => clearInterval(intervalId);
-    const values = localStorage.getItem("values"); //undefined
-    setValue(values || "null"); // without '[]' empty array loop will not occure
+    // const intervalId = setInterval(() => console.log("I am effect"), 100);
+    // return () => clearInterval(intervalId);
+    // const values = localStorage.getItem("values"); //undefined
+    // setValue(values || "null"); // without '[]' empty array loop will not occure
     // for [] array, object -> every time there sets reference that's why infinity loop
     // return () => {
     //   console.log("I am out useEffect");
@@ -49,9 +55,15 @@ const Counter = () => {
     <div className={styles.counter}>
       Counter
       <p>This is Counter : {count}</p>
+      <p>This is Value: {value}</p>
       {/* <button className="button-primary item-1">Button 1</button>*/}
       <Button onClick={handleCount}>Button 1</Button>
-      <button className="button-secondary item-2">Button 2</button>
+      <button
+        className="button-secondary item-2"
+        onClick={() => dispatch("decrement")} // or increment
+      >
+        Button 2
+      </button>
     </div>
   );
 };
